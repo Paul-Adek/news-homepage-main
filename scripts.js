19c85f7e575d753099d67982b4fb1e00923ec3a8
Paul Adekomi <117806363+Paul-Adek@users.noreply.github.com>
2023-01-20 19:30:04 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+var menuButton = document.getElementById("menu");
+var exitButton = document.getElementById("close");
+var menuBox = document.getElementById("menu-box"); 
+
+function menuIcon() {
+    menuBox.style.display = "block";
+};
+
+function closeIcon() {
+    menuBox.style.display = "none";
+};
